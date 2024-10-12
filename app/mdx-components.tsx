c40b59499c440844b0e6dc31735dc71f0d9f7f96
Kohaku233 <2427503528@qq.com>
2024-10-13 00:58:48 +0900

    nextjs 官方 mdx 使用教学，目前出现报错问题

--- app/mdx-components.tsx
+++ app/mdx-components.tsx
@@ -1,7 +1,8 @@
-import type { MDXComponents } from 'mdx/types'
- 
+import type { MDXComponents } from "mdx/types";
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-  }
+    // 你可以在这里添加自定义组件
+  };
 }
