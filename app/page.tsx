2f3e5adbc8b0f94102808160b45f4dc7abcd9cca
mengzhuang-jin <jin.mengzhuang-tcdt@tci-pf.com>
2025-01-15 00:19:02 +0900

    Update framer-motion and related dependencies; implement Tilt component for enhanced UI interactions. Refactor HomePage layout to incorporate Tilt effect on images, improving visual dynamics and user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -2,39 +2,43 @@ import RetroGrid from "@/components/ui/retro-grid";
 import SparklesText from "@/components/ui/sparkles-text";
 import Image from "next/image";
 import BlurFade from "@/components/ui/blur-fade";
+import { Tilt } from "@/components/ui/tilt";
 
 import { porfolio } from "@/lib/resume";
 export default async function HomePage() {
   return (
-      <div className="px-4 sm:px-6 lg:px-8">
-        <div className="relative flex h-[800px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
-          <div className="absolute top-0 right-0 w-[400px] h-[200px]">
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="relative flex h-[800px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
+        <div className="absolute top-0 right-0 w-[400px] h-[200px] perspective-1000">
+          <Tilt rotationFactor={10} isRevese>
             <BlurFade delay={0.1}>
               <Image
                 src="/eva_01.png"
                 alt="EVA01"
                 width={400}
                 height={200}
-                className="object-contain "
+                className="object-contain"
               />
             </BlurFade>
-          </div>
-          <div className="mt-20">
-            <BlurFade>
-              <h1 className="text-4xl font-bold text-black dark:text-white mb-4">
-                {porfolio.intro}
-                <SparklesText className="inline-block" text={porfolio.name} />
+          </Tilt>
+        </div>
+        <div className="mt-20">
+          <BlurFade>
+            <h1 className="text-4xl font-bold text-black dark:text-white mb-4">
+              {porfolio.intro}
+              <SparklesText className="inline-block" text={porfolio.name} />
             </h1>
-            </BlurFade>
-            <h1 className="text-2xl font-bold text-gray-600 dark:text-gray-300 ">
-              {porfolio.description.map((desc, index) => (
-                <BlurFade delay={0.3 + index * 0.1} key={index}>
-                  <p>{desc}</p>
-                </BlurFade>
-              ))}
-            </h1>
-          </div>
-          <div className="absolute bottom-0 left-0 w-[300px] h-[200px]">
+          </BlurFade>
+          <h1 className="text-2xl font-bold text-gray-600 dark:text-gray-300 ">
+            {porfolio.description.map((desc, index) => (
+              <BlurFade delay={0.3 + index * 0.1} key={index}>
+                <p>{desc}</p>
+              </BlurFade>
+            ))}
+          </h1>
+        </div>
+        <div className="absolute bottom-0 left-0 w-[300px] h-[200px] perspective-1000">
+          <Tilt rotationFactor={20} isRevese>
             <BlurFade delay={0.6}>
               <Image
                 src="/eva_02.png"
@@ -44,8 +48,9 @@ export default async function HomePage() {
                 className="object-contain"
               />
             </BlurFade>
-          </div>
-          <RetroGrid />
+          </Tilt>
+        </div>
+        <RetroGrid />
       </div>
     </div>
   );
